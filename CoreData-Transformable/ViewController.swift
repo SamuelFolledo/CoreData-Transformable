53a25bc56d340725bb265e8e2a78ee06dd2ae6b7
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-29 19:03:33 -0400

    [add] final changes on the project

--- CoreData-Transformable/ViewController.swift
+++ CoreData-Transformable/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
             view.backgroundColor = person.favoriteColor
         }
     }
-    var favoriteColor: UIColor?
     lazy var coreDataStack = CoreDataStack(modelName: "CoreData_Transformable")
     
     //MARK: Views
@@ -31,7 +30,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var purpleButton: UIButton!
-
     lazy var saveEditButton: UIBarButtonItem = {
         let barButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(self.handleSavePerson))
         return barButton
@@ -45,15 +43,15 @@ class ViewController: UIViewController {
     }
     
     //MARK: Methods
+    ///Load the first Person from our CoreData
     func loadPerson() {
-        let personFetchRequest: NSFetchRequest<Person> = Person.fetchRequest()
+        let personFetchRequest: NSFetchRequest<Person> = Person.fetchRequest() //make a Person Entity fetch request
         do {
             let results = try coreDataStack.mainContext.fetch(personFetchRequest)
             if results.count > 0 {
-                // cactus found
+                //person found
                 person = results.first
-            } else {
-                // not found, create cactus
+            } else { //no person found
                 person = Person(context: coreDataStack.mainContext)
                 coreDataStack.saveContext()
             }
@@ -62,12 +60,12 @@ class ViewController: UIViewController {
         }
     }
     
-    //MARK: Helpers
+    ///save person to our CoreData
     @objc func handleSavePerson() {
         guard let name = nameTextField.text, !name.isEmpty else { return }
         person!.name = name
         person!.colors = [.red, .orange, .yellow, .green, .blue, .purple]
-        person!.favoriteColor = favoriteColor ?? person!.favoriteColor
+        person!.favoriteColor = person!.favoriteColor
         //save the managed object context
         do {
             try coreDataStack.mainContext.save()
@@ -76,11 +74,12 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func colorButtonTapped(sender: Any) {
+    ///update view's background color with the sender's background color. Update favoriteColor and
+    @objc func colorButtonTapped(_ sender: Any) {
         let button = sender as? UIButton
         guard let color = button?.backgroundColor else { return }
         view.backgroundColor = color
-        favoriteColor = color
+        person!.favoriteColor = color
         button?.layer.borderColor = UIColor.white.cgColor
         for coloredButton in buttons where coloredButton != button {
             coloredButton.layer.borderColor = UIColor.clear.cgColor
@@ -103,7 +102,7 @@ private extension ViewController {
             button.layer.masksToBounds = true
             button.layer.borderWidth = 5
             button.layer.borderColor = UIColor.clear.cgColor
-            button.addTarget(self, action: #selector(colorButtonTapped(sender:)), for: .touchUpInside)
+            button.addTarget(self, action: #selector(colorButtonTapped(_:)), for: .touchUpInside)
         }
         if person!.colors.count == 0 {
             view.backgroundColor = .white
@@ -113,7 +112,6 @@ private extension ViewController {
             button.backgroundColor = person!.colors[index]
             if let favColor = person?.favoriteColor, favColor == button.backgroundColor! {
                 button.layer.borderColor = UIColor.white.cgColor
-                favoriteColor = favColor
             }
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # My CoreData Tutorial: Transformable
-
+Source code for my Medium tutorial [Saving UIColor and Array in CoreData Using Transformable](https://medium.com/@samuelfolledo/saving-uicolor-and-array-in-coredata-using-transformable-4fb906d0a553?sk=111ebc91493fa032d1d698d5dd77dcfb)
